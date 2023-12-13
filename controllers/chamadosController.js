685a0cc013e9c97eac0646f22b4c516181917f6d
Ailton <ailton.souza97@hotmail.com>
2023-12-13 17:46:40 -0300

    fazer descri textarea

--- controllers/chamadosController.js
+++ controllers/chamadosController.js
@@ -2,9 +2,18 @@ const express = require("express");
 const Chamados = require("../models/ChamadosModel")
 const router = express.Router();
 
-router.get("/get_all/:setor", async(req, res)=>{
+router.get("/get_all/:setor/:designado", async(req, res)=>{
     try {
-        res.send(await Chamados.all(req.params.setor))
+        res.send(await Chamados.all(req.params.setor, req.params.designado))
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+});
+
+router.get("/:id", async(req, res)=>{
+    try {
+        res.send(await Chamados.one(req.params.id))
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
--- models/ChamadosModel.js
+++ models/ChamadosModel.js
@@ -22,13 +22,56 @@ async function connect(){
 
 connect();
 
-const all = async(setor)=>{
+const all = async(setor, designado)=>{
     const conn = await connect();
-    const [rows] = await conn.query(`select * from chamados where status not in (6, 3) and chamado_setor_id = ${setor} and ativo = 's' and operacao_id not in (13, 15) order by id desc`);
+    const [rows] = await conn.query(`
+    select chamados.id, users.name, 
+    u_designado.name as designado, 
+    chamados.descricao,
+    areas_atuacaos.descricao as 'area',
+    operacoes.descricao as 'operacoes',
+    chamados.urgencia_id as 'urgencia'
+    from chamados
+    inner join users on chamados.usuario_id = users.id
+    inner join users as u_designado on chamados.designado_id = u_designado.id
+    inner join areas_atuacaos on chamados.area_id = areas_atuacaos.id
+    inner join operacoes on chamados.operacao_id = operacoes.id
+    where chamados.status not in (6, 3) and
+    chamados.chamado_setor_id = ${setor} and
+    chamados.designado_id in(${designado}, 0) and
+    chamados.ativo = 's' and
+    chamados.operacao_id not in (13, 15) and 
+    terceiro_id IS NULL
+    order by chamados.id desc
+    `);
+    conn.end();
+    return rows;
+}
+
+const one = async(id)=>{
+    const conn = await connect();
+    const [rows] = await conn.query(`
+        select chamado_setors.descricao as 'setor',
+        areas_atuacaos.descricao as 'area',
+        operacoes.descricao as 'operacoes',
+        status_chamados.descricao as 'status',
+        urgencias.descricao as 'urgencias',
+        u_designado.name as 'designado'
+        from chamados
+        inner join users on chamados.usuario_id = users.id
+        inner join users as u_designado on chamados.designado_id = u_designado.id
+        inner join chamado_setors on chamados.chamado_setor_id = chamado_setors.id
+        inner join areas_atuacaos on chamados.area_id = areas_atuacaos.id
+        inner join operacoes on chamados.operacao_id = operacoes.id
+        inner join status_chamados on chamados.status = status_chamados.id
+        inner join urgencias on chamados.urgencia_id = urgencias.id
+        where chamados.id = ${id}
+    `);
     conn.end();
     return rows;
 }
 
 module.exports = {
-    all
+    all,
+    one
 };
