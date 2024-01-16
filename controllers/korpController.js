976ad27450de679d2a984ca75d9550fbf47a940c
Ailton <ailton.souza97@hotmail.com>
2024-01-16 17:47:43 -0300

    minor

--- controllers/korpController.js
+++ controllers/korpController.js
@@ -36,4 +36,47 @@ router.get("/produto/:codigo", async(req, res)=>{
     }
 });
 
+router.get("/pedidos-de-compra/get_all", async(req, res)=>{
+    try {
+        res.json(await korpModel.allPedidosDeCompra())
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+});
+
+router.get("/pedidos-de-compra/pesquisa", async(req, res)=>{
+    try {
+        let resultados
+        if(req.query.resultados == 'null' || req.query.resultados == undefined || req.query.resultados == '')
+        {
+            resultados = 1000
+        }else{
+            resultados = req.query.resultados
+        }
+        res.json(await korpModel.searchPedidosDeCompra(req.query.codigo, resultados, req.query.rassoc));
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.get("/pedido-de-compra/:codigo", async(req, res)=>{
+    try {
+        res.json(await korpModel.pedidoDeCompra(req.params.codigo));
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.get("/pedido-de-compra/obs1/:codigo", async(req, res)=>{
+    try {
+        res.json(await korpModel.uncryptObs(req.params.codigo));
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
 module.exports = router;
--- models/korpModel.js
+++ models/korpModel.js
@@ -21,24 +21,56 @@ const all = async()=>{
     const estoqueQuery = `SELECT TOP (1000) ID, CODIGO, DESCRI FROM ESTOQUE`;
     const estoque = await sql.query(estoqueQuery);
     return estoque.recordset;
-}
+};
 
 const search = async(codigo, resultados)=>{
     await sql.connect(sqlConfig);
     const estoqueQuery = `SELECT TOP (${resultados}) CODIGO, DESCRI FROM ESTOQUE WHERE CODIGO LIKE '%${codigo}%' ORDER BY CODIGO`;
     const estoque = await sql.query(estoqueQuery);
     return estoque.recordset;
-}
+};
 
 const product = async(codigo)=>{
   await sql.connect(sqlConfig);
   const productQuery = `SELECT * FROM ESTOQUE WHERE CODIGO = '${codigo}'`;
   const product = await sql.query(productQuery);
   return product.recordset;
-}
+};
+
+const allPedidosDeCompra = async()=>{
+  await sql.connect(sqlConfig);
+  const estoqueQuery = `SELECT TOP (1000) NDOC, RASSOC, REQUISITANTE FROM PEDIDO_FOR ORDER BY NDOC DESC`;
+  const estoque = await sql.query(estoqueQuery);
+  return estoque.recordset;
+};
+
+const searchPedidosDeCompra = async(codigo, resultados, rassoc)=>{
+  await sql.connect(sqlConfig);
+  const estoqueQuery = `SELECT TOP (${resultados}) NDOC, RASSOC, REQUISITANTE FROM PEDIDO_FOR WHERE NDOC LIKE '%${codigo}%' AND RASSOC LIKE '%${rassoc}%' ORDER BY NDOC DESC`;
+  const estoque = await sql.query(estoqueQuery);
+  return estoque.recordset;
+};
+
+const pedidoDeCompra = async(codigo)=>{
+  await sql.connect(sqlConfig);
+  const productQuery = `SELECT * FROM PEDIDO_FOR WHERE NDOC = '${codigo}'`;
+  const product = await sql.query(productQuery);
+  return product.recordset;
+};
+
+const uncryptObs = async(codigo)=>{
+  await sql.connect(sqlConfig);
+  const productQuery = `SELECT CAST(CAST (OBS1 AS varbinary(MAX)) AS VARCHAR(MAX)) as obs FROM PEDIDO_FOR WHERE NDOC = ${codigo}`;
+  const product = await sql.query(productQuery);
+  return product.recordset;
+};
 
 module.exports = {
     all,
     search,
-    product
+    product,
+    allPedidosDeCompra,
+    searchPedidosDeCompra,
+    pedidoDeCompra,
+    uncryptObs
 };
