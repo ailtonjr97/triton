c922182cd2f99c42586ac2e05d490ff3a56fa799
Ailton de Souza Junior <ailtonparanista@hotmail.com>
2024-05-28 22:20:10 -0300

    terminado capos orçamento

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -25,8 +25,6 @@ router.get("/get_all/inactive", async(req, res)=>{
 
 router.get("/:id", async(req, res)=>{
     try {
-        console.log(req.params)
-        console.log(await Users.one(req.params.id))
         res.send(await Users.one(req.params.id));
     } catch (error) {
         console.log(error)
