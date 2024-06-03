8d625f19e3af872114c52d9eeb8564547e364804
Ailton de Souza Junior <ailtonparanista@hotmail.com>
2024-06-02 21:12:42 -0300

    Corrigido git

--- controllers/comercial/orcamentoController.js
+++ controllers/comercial/orcamentoController.js
@@ -66,35 +66,6 @@ async function orcamentoInfo(req, res) {
     }
 }
 
-async function orcamentoInfo(req, res) {
-    try {
-        const filial  = !req.query.filial  ? '' : req.query.filial;
-        const numero  = !req.query.numero  ? '' : req.query.numero;
-        const cliente = !req.query.cliente ? '' : req.query.cliente;
-        const loja    = !req.query.loja    ? '' : req.query.loja;
-
-        const response = await axios.get(`${process.env.APITOTVS}/MODULO_ORC/unico?filial=${filial}&numero=${numero}&cliente=${cliente}&loja=${loja}`, {
-            auth: {
-                username: process.env.USERTOTVS,
-                password: process.env.SENHAPITOTVS
-            }
-        });
-
-        const apiObject = response.data.objects[0]
-
-        res.json({
-            CJ_FILIAL:  apiObject.CJ_FILIAL,
-            CJ_NUM:     apiObject.CJ_NUM,
-            CJ_EMISSAO: convertDateFormat(apiObject.CJ_EMISSAO),
-            CJ_CLIENTE: apiObject.CJ_CLIENTE,
-            CJ_LOJA:    apiObject.CJ_LOJA
-        });
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-}
-
 async function cliente(req, res) {
     try {
         const cliente  = !req.query.cliente  ? '' : req.query.cliente;
